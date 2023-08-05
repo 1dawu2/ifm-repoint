76dbc68bd60af04a3a97a950e9a46738b5163862
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-08-05 13:48:01 +0200

    Update ifm-repoint.js

--- ifm-repoint.js
+++ ifm-repoint.js
@@ -296,7 +296,7 @@
                         };
 
                         var xhr = new XMLHttpRequest();
-                        // xhr.withCredentials = true;
+                        xhr.withCredentials = true;
 
                         xhr.addEventListener("readystatechange", function () {
                             if (this.readyState === 4) {
@@ -305,12 +305,11 @@
                         });
 
                         xhr.open("POST", "https://infomotion1.eu10.hanacloudservices.cloud.sap/sap/fpa/services/rest/epm/contentlib?tenant=K");
-                        // xhr.setRequestHeader("x-csrf-token", "839e9753-a8eb-4f51-af22-65fcb84fbc11");
                         // WARNING: Cookies will be stripped away by the browser before sending the request.
-                        // xhr.setRequestHeader("Cookie", "s:IBGXzjjviOIwz7NyjNX4SLVj5bYswc5x.Ch8F1wvNx1dJ947DA5vfusaoar4Iow9XCZKCv0ez33w");
-                        // xhr.setRequestHeader("x-sap-sac-custom-auth", "true");
-                        // xhr.setRequestHeader("x-csrf-token", FPA_CSRF_TOKEN);
-                        // xhr.setRequestHeader("Content-Type", "application/json");
+                        xhr.setRequestHeader("Cookie", "s:IBGXzjjviOIwz7NyjNX4SLVj5bYswc5x.Ch8F1wvNx1dJ947DA5vfusaoar4Iow9XCZKCv0ez33w");
+                        xhr.setRequestHeader("x-sap-sac-custom-auth", "true");
+                        xhr.setRequestHeader("x-csrf-token", FPA_CSRF_TOKEN);
+                        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
                         xhr.send(data);
 
