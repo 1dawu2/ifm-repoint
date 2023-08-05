7e122992df871def1df2ac4c9e35605ee1ed0529
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-08-05 13:19:44 +0200

    Update ifm-repoint.js

--- ifm-repoint.js
+++ ifm-repoint.js
@@ -309,7 +309,7 @@
                         // WARNING: Cookies will be stripped away by the browser before sending the request.
                         // xhr.setRequestHeader("Cookie", "s:IBGXzjjviOIwz7NyjNX4SLVj5bYswc5x.Ch8F1wvNx1dJ947DA5vfusaoar4Iow9XCZKCv0ez33w");
                         xhr.setRequestHeader("x-sap-sac-custom-auth", "true");
-                        xhr.setRequestHeader("x-csrf-token", this.FPA_CSRF_TOKEN);
+                        xhr.setRequestHeader("x-csrf-token", FPA_CSRF_TOKEN);
                         xhr.setRequestHeader("Content-Type", "application/json");
 
                         xhr.send(data);
