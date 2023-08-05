a6b4123bad120e75d0fc28ffcfa87ebb21049813
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-08-05 14:27:43 +0200

    Update ifm-repoint.js

--- ifm-repoint.js
+++ ifm-repoint.js
@@ -463,13 +463,14 @@
                                     var oData = sap.ui.getCore().getModel().oData;
                                     that_.updateList(oData);
                                     this.oDefaultDialog.close();
-                                    this.getStoryInfo("179AF700C1F6054D4DB416C623EE5D2B").then(function (content) {
-                                        var entities = this.getModelList(content);
-                                    }).catch(function (error) {
-                                        console.log(error);
-                                    });
+                                    // this.getStoryInfo("179AF700C1F6054D4DB416C623EE5D2B").then(function (content) {
+                                    //     var entities = this.getModelList(content);
+                                    // }).catch(function (error) {
+                                    //     console.log(error);
+                                    // });
 
                                     var resp = getStoryInfo("179AF700C1F6054D4DB416C623EE5D2B");
+                                    console.log(resp);
                                     // this.getStoryContent("179AF700C1F6054D4DB416C623EE5D2B").then(function (response) {
                                     //     var resourceInfoStory = JSON.stringify(response);
                                     //     var entities = this.getModelList(response);
